91b88c4a18df1797f984d472e2175ab730a5b799
unknown <dashing.hank@gmail.com>
2022-05-23 15:44:09 +0800

    updated6

--- api/test.ts
+++ api/test.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 export default async (request: VercelRequest, response: VercelResponse) => {
   // console.log(request);
 
-  console.log(request.body.message);
+  console.log(request.body);
 
   response.status(200).send("Hello World!");
 };
