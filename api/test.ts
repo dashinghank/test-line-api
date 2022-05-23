9ea199f6e0234f0f7576f6c24416864818fddb8f
unknown <dashing.hank@gmail.com>
2022-05-23 15:46:54 +0800

    updated7

--- api/test.ts
+++ api/test.ts
@@ -2,7 +2,25 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 export default async (request: VercelRequest, response: VercelResponse) => {
   // console.log(request);
 
-  console.log(request.body);
+  /* request 範例
+  {
+    destination: 'U17b18a19a489476822697f1ad9e99dd6',
+    events: [
+      {
+        type: 'message',
+        message: [Object],
+        webhookEventId: '01G3QY1NBDK7KF418W83417Q2D',
+        deliveryContext: [Object],
+        timestamp: 1653291930631,
+        source: [Object],
+        replyToken: '4bf840e5dbb84854bed05d81cacb9758',
+        mode: 'active'
+      }
+    ]
+  }
+*/
+
+  console.log(request.body.events[0].message);
 
   response.status(200).send("Hello World!");
 };
