e870ee737a637580cd3285bc5a4d65d23a1e738f
unknown <dashing.hank@gmail.com>
2022-05-24 11:42:33 +0800

    line bot

--- api/test.ts
+++ api/test.ts
@@ -1,25 +1,45 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import * as line from "@line/bot-sdk";
+const config = {
+  channelAccessToken: "37nq0gcLXN1aV/159qmuXxjqRR0nDx3nAbfYsDlxlOHvTnKS0ERtA+wFfX7FVQfe8XrS/f4TETyR6szRDZA95xsYFrM1iGMle5riPJz+ul6iBokN7s6+CxtiwR2TJHuSqIqkz5RcvWBtw//beGKTXQdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "c912a81ed58c085cdd4b5794ead81b25",
+};
+
 export default async (request: VercelRequest, response: VercelResponse) => {
+  Promise.all(request.body.events.map(handleEvent)).then((result) => response.json(result));
   // console.log(request);
 
   /* request 範例
   {
-    destination: 'U17b18a19a489476822697f1ad9e99dd6',
+    destination: "U17b18a19a489476822697f1ad9e99dd6",
     events: [
       {
-        type: 'message',
-        message: [Object],
-        webhookEventId: '01G3QY1NBDK7KF418W83417Q2D',
-        deliveryContext: [Object],
-        timestamp: 1653291930631,
-        source: [Object],
-        replyToken: '4bf840e5dbb84854bed05d81cacb9758',
-        mode: 'active'
-      }
-    ]
-  }
+        type: "message",
+        message: { type: "text", id: "16135080271826", text: "我" },
+        webhookEventId: "01G3QY996M13KEFBVC31YEJ9MG",
+        deliveryContext: { isRedelivery: false },
+        timestamp: 1653292180445,
+        source: { type: "user", userId: "U4a753e22048f8e07be32962f1a96b642" },
+        replyToken: "2b0f7757ce55435db1a071cbd4d0298e",
+        mode: "active",
+      },
+    ],
+  };
 */
+
   console.log(JSON.stringify(request.body));
 
   response.status(200).send("Hello World!");
 };
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: "這是我的回覆:" + event.message.text,
+  });
+}
